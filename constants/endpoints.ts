211dbbfef5b26fdbd29b9b7f70053e46248a2faa
Pasindu Piyathilaka <ppiyathilaka@saveenergysystems.com>
2025-08-25 13:05:16 +0530

    fix: restore endpoints.ts

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -1,4 +1,7 @@
-export const MAINNET_RPC = process.env.NEXT_PUBLIC_MAINNET_RPC ||  process.env.MAINNET_RPC ||'https://api.devnet.solana.com'
+export const MAINNET_RPC =
+  process.env.NEXT_PUBLIC_MAINNET_RPC ||
+  process.env.MAINNET_RPC ||
+  'http://realms-realms-c335.mainnet.rpcpool.com'
 
 export const DEVNET_RPC =
   process.env.NEXT_PUBLIC_DEVNET_RPC ||
